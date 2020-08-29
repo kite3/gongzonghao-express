1ad16ffe3792e42897c09ebfe1a12fe82f6502f1
kite3 <iskit3@126.com>
2020-08-29 20:47:12 +0800

    编写/authwx路由，验证消息是否来自微信服务器

---
+++ config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  appId: 'wxac66218a8d2ab517',
+  appSecret: '2bae25b2e0e9a5d620aa64af711da6b2',
+  token: 'testWeixin'
+}
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,31 @@
 var express = require('express')
 var router = express.Router()
+var sha1 = require('sha1')
 var userModel = require('../db/models/Usermodel')
+const config = require('../config')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express 123888' })
 })
+
+// 验证消息是否来自微信服务器
+router.get('/authwx', function (req, res, next) {
+  const { signature, timestamp, nonce, echostr } = req.query
+  const { token } = config
+  let array = [timestamp, nonce, token]
+  array.sort() // 字典排序
+  let str = sha1(array.join(''))
+  if (str === signature) {
+    res.set('Content-Type', 'text/plain')
+    res.send(echostr)
+  } else {
+    res.send('Error！不是来自微信服务器!!!!!')
+  }
+})
+
+// 获取js-sdk中wx.config所需的参数
+router.get('/jsapi', function (req, res, next) {})
 
 router.post('/testdb', function (req, res, next) {
   const { user, pwd } = req.body
